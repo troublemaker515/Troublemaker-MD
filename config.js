74a13cd9fd3dc7b0fbc806c3f3296f4c0173920f
Vivek Kp <85311774+troublemaker515@users.noreply.github.com>
2022-12-08 13:09:02 +0530

    Update config.js
    commit

--- config.js
+++ config.js
@@ -6,17 +6,17 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
 global.port= process.env.PORT || 5000
-global.email = 'sam@secktor.live'
-global.github = 'https://github.com/SamPandey001/Secktor-Md'
+global.email = 'troublemakerz437@gmail.com'
+global.github = 'https://github.com/troublemaker515/Troublemaker-MD'
 global.location = 'bangalore IN'
 global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '919628516236'
-global.devs = '919628516236';
-global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+91000000000000
+global.sudo = process.env.SUDO || '919148942515'
+global.devs = '919148942515';
+global.website = 'https://github.com/troublemaker515/Troublemaker-MD' //wa.me/+91000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
 module.exports = {
-  botname: process.env.BOT_NAME || '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨',
-  ownername:process.env.OWNER_NAME || 'SamPandey001',
+  botname: process.env.BOT_NAME || 'Troublemaker 𝐁𝐨𝐭𝐭𝐨',
+  ownername:process.env.OWNER_NAME || 'troublemaker515',
   sessionName: process.env.SESSION_ID || 'PUT-HERE',
   author: process.env.PACK_INFO.split(";")[0] || 'author', 
   packname: process.env.PACK_INFO.split(";")[1] || 'Name',
